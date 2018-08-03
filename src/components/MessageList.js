7481e88ed0a86d67f613946ede6e3bbc24575639
OneOfOne <oneofone@gmail.com>
2018-08-03 22:45:52 +0200

    allow passing the icon to messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import Message from './Messages'
+import Message from './Messages';
 
 class MessageList extends Component {
+	componentDidUpdate(prevProps, prevState) {
+		this.scrollList.scrollTop = this.scrollList.scrollHeight;
+	}
 
-  componentDidUpdate(prevProps, prevState) {
-    this.scrollList.scrollTop = this.scrollList.scrollHeight;
-  }
-
-  render () {
-    return (
-      <div className="sc-message-list" ref={el => this.scrollList = el}>
-        {this.props.messages.map((message, i) => {
-          return <Message message={message} key={i} />
-        })}
-      </div>)
-  }
+	render() {
+		return (
+			<div className="sc-message-list" ref={(el) => this.scrollList = el}>
+				{this.props.messages.map((message, i) => <Message message={message} key={i} />)}
+			</div>
+		);
+	}
 }
 
-export default MessageList
+export default MessageList;
--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react'
-import TextMessage from './TextMessage'
-import EmojiMessage from './EmojiMessage'
-import chatIconUrl from './../../assets/chat-icon.svg'
-
+import React, { Component } from 'react';
+import TextMessage from './TextMessage';
+import EmojiMessage from './EmojiMessage';
 
 class Message extends Component {
+	_renderMessageOfType(type) {
+		switch (type) {
+			case 'text':
+				return <TextMessage {...this.props.message} />;
+			case 'emoji':
+				return <EmojiMessage {...this.props.message} />;
+		}
+	}
 
-  _renderMessageOfType(type) {
-    switch(type) {
-      case 'text':
-        return <TextMessage {...this.props.message} />
-      case 'emoji':
-        return <EmojiMessage {...this.props.message} />
-    }
-  }
+	render() {
+		const
+			{ author, icon, type } = this.props.message,
+			contentClassList = [
+				'sc-message--content',
+				(author === 'me' ? 'sent' : 'received'),
+			],
+			style = { backgroundImage: `url(${icon || 'https://dash.pathdna.com/images/userIcon.png'})` };
 
-  render () {
-    let contentClassList = [
-      "sc-message--content",
-      (this.props.message.author === "me" ? "sent" : "received")
-    ];
-    return (
-      <div className="sc-message">
-        <div className={contentClassList.join(" ")}>
-          <div className="sc-message--avatar" style={{
-            backgroundImage: `url(${chatIconUrl})`
-          }}></div>
-          {this._renderMessageOfType(this.props.message.type)}
-        </div>
-      </div>)
-  }
+		return (
+			<div className="sc-message">
+				<div className={contentClassList.join(' ')}>
+					<div className="sc-message--avatar" style={style} />
+					{this._renderMessageOfType(type)}
+				</div>
+			</div>);
+	}
 }
 
-export default Message
+export default Message;
